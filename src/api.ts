197ffd6fa9811b5d9deb516faf0c4f70ce499337
Shashika Virajh <shashikasvka@gmail.com>
2023-04-24 22:40:45 +0530

    Axios retry feature

--- src/api.ts
+++ src/api.ts
@@ -2,34 +2,44 @@ import axios, { AxiosInstance, AxiosError } from 'axios';
 import { useSelector } from 'react-redux';
 import { AppDispatch, RootState, store } from './store/store';
 import { refreshAccessToken, setAccessToken, setRefreshToken } from './store/auth-slice';
+import axiosRetry from 'axios-retry';
 
 type ResponseData = {
   data: any;
 };
 
-type TokenResponse = {
-  data: {
-    accessToken: string;
-    refreshToken: string;
-  };
+type TErrorResponse = {
+  error: string;
+  message: string;
 };
 
+type TApiError = AxiosError<TErrorResponse>;
+
+const API_BASE_URL = process.env.REACT_APP_API_URL;
+
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: API_BASE_URL,
+  timeout: 30000,
+  withCredentials: true, // Allow to send cookies with requests
   headers: {
     'Content-Type': 'application/json'
   }
 });
 
-axiosInstance.interceptors.request.use((config) => {
-  const accessToken = useSelector((state: RootState) => state.auth.accessToken);
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = useSelector((state: RootState) => state.auth.accessToken);
 
-  if (accessToken) {
-    config.headers.Authorization = `Bearer ${accessToken}`;
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  (error: TApiError) => {
+    return Promise.reject(error);
   }
-
-  return config;
-});
+);
 
 axiosInstance.interceptors.response.use(
   // If the response is a success
@@ -40,20 +50,18 @@ axiosInstance.interceptors.response.use(
   async (error: AxiosError) => {
     const dispatch = store.dispatch as AppDispatch;
 
-    // const originalRequest = error?.config;
+    const refreshToken = useSelector((state: RootState) => state.auth.refreshToken) ?? '';
 
     if (error.response?.status === 401) {
-      const refreshToken = useSelector((state: RootState) => state.auth.refreshToken) ?? '';
-      const accessToken = useSelector((state: RootState) => state.auth.accessToken) ?? '';
-
       try {
-        // Request to refresh the access token using current refresh token.
-        // Once the tokens are received, they are stored in the auth slice.
-        dispatch(refreshAccessToken(refreshToken));
+        // Request a new access token using the current refresh token.
+        // Once the tokens are received, they are stored in the auth reducer.
+        await dispatch(refreshAccessToken(refreshToken));
+
+        const accessToken = useSelector((state: RootState) => state.auth.accessToken) ?? '';
 
         // Resend the original request with the new access token.
         const originalRequest = error.config;
-
         if (!originalRequest) throw error;
 
         originalRequest.headers.Authorization = `Bearer ${accessToken}`;
@@ -72,4 +80,10 @@ axiosInstance.interceptors.response.use(
   }
 );
 
-export default axiosInstance;
+axiosRetry(axiosInstance, {
+  retries: 3,
+  retryDelay: (retryCount: number) => retryCount * 1000,
+  retryCondition: (error: AxiosError) => error.code === 'ETIMEDOUT' // Only retry on timeout errors
+});
+
+export { axiosInstance };
